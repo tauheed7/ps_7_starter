45f8a0d29fc8d6c5f346f9412b92656bdad167ef
Tauheed Islam <tauheed_islam@college.harvard.edu>
2018-11-23 09:51:07 -0500

    app up and running

--- ps_7/app.R
+++ ps_7/app.R
@@ -8,43 +8,75 @@
 #
 
 library(shiny)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+all_data <- read_rds("ps_7_data.rds")
+
+education_choices <- all_data %>% 
+  group_by(educ) %>% 
+  summarize()
+
+location_choices <- all_data %>% 
+  group_by(join) %>% 
+  summarize()
+
+ui <- fluidPage(  
+                 
+                 titlePanel("Effectiveness of Poll Predictions based on Voter Education"),
+                 
+                 sidebarLayout(
+                   sidebarPanel(
+                     selectInput(inputId = "join",
+                                 label = "State & District:",
+                                 choices = location_choices,
+                                 selected = "AZ-06"),
+                     selectInput(inputId = "education",
+                                 label = "Education:",
+                                 choices = education_choices,
+                                 selected = "High school")
+                   ),
+                   
+                   mainPanel(
+                     tabsetPanel(type = "tabs",
+                                 tabPanel("About This App", 
+                                          h1("Summary"),
+                                          p("This app examines what the NYT Upshot polls got right and wrong about the vote based on voters' education. 
+                                            Essentially, it measures the degree to which the polls were similar to the actual results via bar graphs."),
+                                          p("You can examine the polled results vs. the actual results by district and level of education on the graphics tab"),
+                                          p("To navigate, simply click on the drop down menu of each desired variable to select your desired observation.")),
+                                 tabPanel("Graphic", plotOutput("plot")))
+                                 )
+                   )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$plot <- renderPlot({
+    
+    req(input$education, input$join)
+    
+    data <- all_data %>% 
+      filter(educ == input$education, join == input$join)
+    
+    bar_data <- data.frame(
+      type = factor(c("Polled Result", "Polled Result", "Polled Result", "Actual Result", "Actual Result", "Actual Result")),
+      party = factor(c("Republican", "Democratic", "Other", "Republican", "Democratic", "Other"),
+                     levels=c("Republican", "Democratic", "Other")),
+      percent = c(data$predicted_rep,
+                  data$predicted_dem,
+                  data$predicted_other,
+                  data$real_rep, 
+                  data$real_dem,
+                  data$real_other)
+    )
+    
+    ggplot(data = bar_data, aes(x = party, y = percent, fill = type)) +
+      geom_bar(color = "black", stat = "identity", position = position_dodge()) +
+      xlab("Party") + ylab("Percentage of Vote") +
+      ggtitle("Percentage of Vote based on Education") +
+      theme_classic()
+  })
+  
 }
-
-# Run the application 
+# Running the application here 
 shinyApp(ui = ui, server = server)
-
